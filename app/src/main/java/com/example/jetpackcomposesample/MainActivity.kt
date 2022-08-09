f1293b49e2559a4fd1badc138fb83489bc31e033
Ragul Mohan <ragul.m@tringapps.com>
2022-08-09 22:34:44 +0530

    Jetpack Compose - List using Lazy Column

--- app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.jetpackcomposesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -41,41 +40,23 @@ import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-    val fonts = FontFamily(
-        Font(R.font.oleoscriptbold, FontWeight.Bold),
-        Font(R.font.oleoscriptregular, FontWeight.Normal),
-    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
-            val scaffoldState = rememberScaffoldState()
-            val coroutineScope = rememberCoroutineScope()
-            Scaffold(modifier = Modifier.fillMaxSize(), scaffoldState = scaffoldState) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    TextField(
-                        value = textFieldState,
-                        label = { "Enter your name" },
-                        onValueChange = {
-                            textFieldState = it
-                        }, singleLine = true, modifier = Modifier.fillMaxWidth()
+            val scrollState = rememberScrollState()
+            LazyColumn {
+                items(500){
+                    Text(
+                        text = "$it",
+                        fontSize = 25.sp,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(vertical = 20.dp)
+                            .fillMaxWidth()
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        coroutineScope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
-                        }
-
-                    }) {
-                        Text("Please Click Me")
-                    }
                 }
             }
         }
