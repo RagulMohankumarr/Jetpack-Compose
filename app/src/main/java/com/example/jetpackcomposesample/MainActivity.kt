96acf82413bceaac43f4abe93948ff8ed2e77ee8
Ragul Mohan <ragul.m@tringapps.com>
2022-08-11 21:04:51 +0530

    Jetpack Compose - Simple Animations

--- app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.jetpackcomposesample
 
+import android.animation.Keyframe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.scrollable
@@ -18,7 +21,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
@@ -49,28 +54,36 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            val constraints = ConstraintSet {
-                var greenBox = createRefFor("greenBox")
-                var redBox = createRefFor("redBox")
-                val guidline = createGuidelineFromTop(0.5f)
-                constrain(greenBox) {
-                    top.linkTo(guidline)
-                    start.linkTo(parent.start)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                constrain(redBox){
-                    top.linkTo(parent.top)
-                    start.linkTo(greenBox.end)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                createHorizontalChain(greenBox,redBox, chainStyle = ChainStyle.Packed)
+            var sizeState by remember {
+                mutableStateOf(200.dp)
             }
-            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
-                Box(modifier = Modifier.background(Color.Green).layoutId("greenBox"))
-                Box(modifier = Modifier.background(Color.Red).layoutId("redBox"))
+            val animateState by animateDpAsState(targetValue = sizeState, keyframes {
+                durationMillis = 5000
+                sizeState at 0 with LinearEasing
+                sizeState * 1.5f at 1000 with FastOutLinearInEasing
+                sizeState * 2f at 5000
+            })
+            val infiniteState = rememberInfiniteTransition()
+            val color by infiniteState.animateColor(
+                initialValue = Yellow,
+                targetValue = Blue,
+                animationSpec = infiniteRepeatable(
+                    tween(1000),
+                    repeatMode = RepeatMode.Reverse
+                )
+
+            )
+            Box(
+                modifier = Modifier
+                    .size(animateState)
+                    .background(color), contentAlignment = Alignment.Center
+            )
+            {
+                Button(onClick = { sizeState += 50.dp }) {
+                    Text("Animate Me")
+                }
             }
         }
     }
