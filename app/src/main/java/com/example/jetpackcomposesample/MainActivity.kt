c1f30c99f83bb423c1d6cc04ef73b97dba3c7b82
Ragul Mohan <ragul.m@tringapps.com>
2022-08-11 20:13:33 +0530

    Jetpack Compose - Constraint Layout

--- app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -35,6 +36,10 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.example.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 import kotlinx.coroutines.launch
 import kotlin.random.Random
@@ -45,19 +50,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scrollState = rememberScrollState()
-            LazyColumn {
-                items(500){
-                    Text(
-                        text = "$it",
-                        fontSize = 25.sp,
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier
-                            .padding(vertical = 20.dp)
-                            .fillMaxWidth()
-                    )
+            val constraints = ConstraintSet {
+                var greenBox = createRefFor("greenBox")
+                var redBox = createRefFor("redBox")
+                val guidline = createGuidelineFromTop(0.5f)
+                constrain(greenBox) {
+                    top.linkTo(guidline)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
                 }
+                constrain(redBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+                createHorizontalChain(greenBox,redBox, chainStyle = ChainStyle.Packed)
+            }
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.background(Color.Green).layoutId("greenBox"))
+                Box(modifier = Modifier.background(Color.Red).layoutId("redBox"))
             }
         }
     }
