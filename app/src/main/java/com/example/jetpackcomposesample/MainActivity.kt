96bd04acc8784721bc7a3f6984d375d92801aa80
Ragul Mohan <ragul.m@tringapps.com>
2022-08-17 21:07:47 +0530

    Jetpack Compose - 3D animated drop down menu

--- app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,17 +26,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.Yellow
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInteropFilter
@@ -76,124 +79,89 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            Surface(
+                color = Color.Black,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                DropDown(
+                    text = "Hello World",
+                    modifier = Modifier.padding(15.dp)
+                ) {
+                    Text(
+                        text = "This is now revealed",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .background(
+                                Green
+                            )
+                    )
+
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun DropDown(
+        text: String,
+        modifier: Modifier = Modifier,
+        initiallyOpened: Boolean = false,
+        content: @Composable () -> Unit
+    ) {
+        var isOpen by remember {
+            mutableStateOf(initiallyOpened)
+        }
+        var alpha = animateFloatAsState(
+            targetValue = if (isOpen) 1f else 0f,
+            animationSpec = tween(
+                durationMillis = 300
+            )
+        )
+        var rotateX = animateFloatAsState(
+            targetValue = if (isOpen) 0f else -90f,
+            animationSpec = tween(
+                durationMillis = 300
+            )
+        )
+        Column(
+            modifier = modifier.fillMaxSize()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = text,
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Open or Close the drop down",
+                    tint = Color.White,
+                    modifier = Modifier
+                        .clickable {
+                            isOpen = !isOpen
+                        }
+                        .scale(1f, if (isOpen) -1f else 1f)
+
+                )
+            }
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black)
+                    .fillMaxWidth()
+                    .graphicsLayer {
+                        transformOrigin = TransformOrigin(0.5f, 0f)
+                        rotationX = rotateX.value
+                    }
+                    .alpha(alpha.value)
             ) {
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .border(1.dp, Color.Green, RoundedCornerShape(10.dp))
-                        .padding(30.dp)
-                ) {
-                    var volume by remember {
-                        mutableStateOf(0f)
-                    }
-                    var barCount = 20
-                    MusicKnob(
-                        modifier = Modifier.size(100.dp)
-                    ) {
-                        volume = it
-                    }
-                    Spacer(modifier = Modifier.width(20.dp))
-                    VolumeBar(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .height(30.dp),
-                        activeBars = (barCount * volume).roundToInt(),
-                        barCount = barCount
-                    )
-                }
+                content()
             }
         }
-    }
-
-    @Composable
-    fun VolumeBar(
-        modifier: Modifier = Modifier,
-        activeBars: Int = 0,
-        barCount: Int = 10
-    ) {
-        BoxWithConstraints(
-            contentAlignment = Alignment.Center,
-            modifier = modifier
-        ) {
-            val barWidth = remember {
-                constraints.maxWidth / (2f * barCount)
-            }
-            Canvas(modifier = modifier) {
-                for (i in 0 until barCount) {
-                    drawRoundRect(
-                        color = if (i in 0..activeBars) Color.Green else Color.DarkGray,
-                        topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
-                        size = Size(barWidth, constraints.maxHeight.toFloat()),
-                        cornerRadius = CornerRadius(0f)
-                    )
-                }
-            }
-        }
-    }
-
-    @OptIn(ExperimentalComposeUiApi::class)
-    @Composable
-    fun MusicKnob(
-        modifier: Modifier = Modifier,
-        limitingAngle: Float = 25f,
-        onValueChange: (Float) -> Unit
-    ) {
-        var rotation by remember {
-            mutableStateOf(limitingAngle)
-        }
-        var touchX by remember {
-            mutableStateOf(0f)
-        }
-        var touchY by remember {
-            mutableStateOf(0f)
-        }
-        var centerX by remember {
-            mutableStateOf(0f)
-        }
-        var centerY by remember {
-            mutableStateOf(0f)
-        }
-        Image(
-            painter = painterResource(id = R.drawable.music_knob),
-            contentDescription = "Music Knob",
-            modifier = modifier
-                .fillMaxSize()
-                .onGloballyPositioned {
-                    val windowBounds = it.boundsInWindow()
-                    centerX = windowBounds.size.width / 2f
-                    centerY = windowBounds.size.height / 2f
-                }
-                .pointerInteropFilter { event ->
-                    touchX = event.x
-                    touchY = event.y
-                    val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
-                    when (event.action) {
-                        MotionEvent.ACTION_MOVE, MotionEvent.ACTION_DOWN -> {
-                            if (angle !in -limitingAngle..limitingAngle) {
-                                val fixedAngle = if (angle in -180f..-limitingAngle) {
-                                    360f + angle
-                                } else {
-                                    angle
-                                }
-                                rotation = fixedAngle
-                                val percent =
-                                    (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
-                                onValueChange(percent)
-                                true
-                            } else false
-                        }
-                        else ->
-                            false
-                    }
-                }
-                .rotate(rotation)
-        )
     }
 }
 
